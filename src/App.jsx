a5931f23510b688c45011db19faaa794ad593be0
Samir Fernandez <samirfdezc@gmail.com>
2024-03-12 14:55:10 -0400

    ++

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Navbar } from "./components/navbar/Navbar";
 import { MapControl } from "./components/maps/MapControl";
+import { PolygonMap } from "./components/maps/PolygonMap";
 import "./App.css";
 
 function App() {
   return (
     <>
       <Navbar />
-      <MapControl />
+      <PolygonMap />
+      {/* <MapControl /> */}
     </>
   );
 }
